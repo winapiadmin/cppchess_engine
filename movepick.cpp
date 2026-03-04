5d8846f6835c843295a8ed2328fadae3ce72c20f
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2026-03-04 13:00:32 +0700

    Update movepick.cpp

--- movepick.cpp
+++ movepick.cpp
@@ -1,10 +1,11 @@
 #include "movepick.h"
 #include "eval.h"
 #include <position.h>
+#include <algorithm>
 using namespace chess;
 namespace engine::movepick {
     Value historyHeuristic[SQUARE_NB][SQUARE_NB]{};
-    Move killerMoves[MAX_PLY][2];
+    Move killerMoves[64][2];
     void orderMoves(chess::Board & board, chess::Movelist & moves, chess::Move ttMove, int ply)
     {
         std::vector<std::pair<chess::Move, Value>> scoredMoves;
