87018d5bed169d0cf2d7bb5d0112043b6ef03033
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2026-03-04 12:04:19 +0700

    Update movepick.h

--- movepick.h
+++ movepick.h
@@ -2,4 +2,6 @@
 #include <fwd_decl.h>
 namespace engine::movepick {
 	void orderMoves(chess::Board &, chess::Movelist &, chess::Move, int);
+	extern Value historyHeuristic[64][64]{};
+    extern Move killerMoves[256][2];
 }
