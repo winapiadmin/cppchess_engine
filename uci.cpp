4d4e1ab7d2e8aa46c1ba15472c30172176fd86e1
winapiadmin <138602885+winapiadmin@users.noreply.github.com>
2026-06-01 16:42:09 +0700

    Fix FEN setting in engine loop

--- uci.cpp
+++ uci.cpp
@@ -158,7 +158,7 @@ void engine::report(std::string_view bestmove) {
 }
 void engine::loop() {
   std::string line;
-  pos.setFen(pos.START_FEN);
+  pos.setFEN(chess::PositionSTART_FEN);
 
   while (std::getline(std::cin, line)) {
     std::istringstream ss(line);
